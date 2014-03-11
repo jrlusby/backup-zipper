874359c293a000a007c3b541763db050818b1717
John Lusby <jlusby42@gmail.com>
2014-03-11 10:44:57 -0700

    screw that try

--- FTPSync.py
+++ FTPSync.py
@@ -24,35 +24,32 @@ def main(argv):
 
 def ftp_sync(local_dir, target_dir, destination, username, password):
     from ftplib import FTP
-    try:
-        #connect to ftp server
-        ftp = FTP(destination)
-        ftp.login(user=username, passwd=password)
-        #select target directory
-        ftp.cwd(target_dir)
-        #index the files in remote and local directories
-        local_files = os.listdir(local_dir)
-        print("local files: ", local_files)
-        ftp.retrlines('LIST', callback=populate_remote)
-        print("remote files: ", remote_files)
-        #create file diffs
-        local_only = list(set(local_files) - set(remote_files))
-        print("local only files: ", local_only)
-        remote_only = list(set(remote_files) - set(local_files))
-        print("remote only files: ", remote_only)
-        #copy local only files to remote
-        print("storing")
-        for file in local_only:
-            print(file)
-            with open(os.path.join(local_dir, file), 'rb') as binary_file:
-                ftp.storbinary('STOR ' + file, binary_file)
-        #delete remote only files from remote
-        print("deleting")
-        for file in remote_only:
-            print(file)
-            ftp.delete(file)
-    except:
-        print('wtf')
+    #connect to ftp server
+    ftp = FTP(destination)
+    ftp.login(user=username, passwd=password)
+    #select target directory
+    ftp.cwd(target_dir)
+    #index the files in remote and local directories
+    local_files = os.listdir(local_dir)
+    print("local files: ", local_files)
+    ftp.retrlines('LIST', callback=populate_remote)
+    print("remote files: ", remote_files)
+    #create file diffs
+    local_only = list(set(local_files) - set(remote_files))
+    print("local only files: ", local_only)
+    remote_only = list(set(remote_files) - set(local_files))
+    print("remote only files: ", remote_only)
+    #copy local only files to remote
+    print("storing")
+    for file in local_only:
+        print(file)
+        with open(os.path.join(local_dir, file), 'rb') as binary_file:
+            ftp.storbinary('STOR ' + file, binary_file)
+    #delete remote only files from remote
+    print("deleting")
+    for file in remote_only:
+        print(file)
+        ftp.delete(file)
 
 def populate_remote(line):
     filename = line.split('0')[-1]
